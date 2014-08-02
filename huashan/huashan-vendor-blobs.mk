028defe52dd51791c35b69c23462a46a9c1c8e04
Ultrax5 <stefan.ultrax5@gmail.com>
2014-08-02 15:07:34 +0200

    Add sysmon to vendor makefile
    *ART is fixed with the version I added, so we can safely use this.

--- huashan/huashan-vendor-blobs.mk
+++ huashan/huashan-vendor-blobs.mk
@@ -306,7 +306,10 @@ PRODUCT_COPY_FILES += \
     vendor/sony/huashan/proprietary/lib/modules/spidev.ko:system/lib/modules/spidev.ko \
     vendor/sony/huashan/proprietary/lib/modules/test-iosched.ko:system/lib/modules/test-iosched.ko \
     vendor/sony/huashan/proprietary/lib/modules/texfat.ko:system/lib/modules/texfat.ko
-
+    
+PRODUCT_COPY_FILES += \
+    vendor/sony/huashan/proprietary/framework/com.sonyericsson.sysmon.jar:system/bin/com.sonyericsson.sysmon.jar
+    
 PRODUCT_COPY_FILES += \
     vendor/sony/huashan/proprietary/bin/wipedata:system/bin/wipedata
 
